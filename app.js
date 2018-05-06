30d263ff50137cfa96a1b1ed4ffad83fd3c17a02
Alex Recker <alex@reckerfamily.com>
2018-05-06 14:33:53 -0500

    Stuff

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
 var app = new Vue({
   el: '#app',
+
   data: {
-    message: 'Hello Vue!',
-    dynamicMessage: 'You loaded this page on ' + new Date().toLocaleString()
+    newHashtag: '',
+    chosenHashtags: [],
+    availableHashtags: [
+      '#astuaryart', '#madisonart', '#art',
+      '#365daysofart', '#madisonartscene',
+      '#springart', '#midwestart', '#liquidart',
+      '#Astuary', '#ArtByMarissaRecker', '#marissarecker', '#instagramshill'
+    ]
+  },
+
+  methods: {
+    alphabetized: function(list) {
+      return list;
+    },
+
+    addNewHashtag: function() {
+      this.chosenHashtags.push(this.newHashtag);
+      this.newHashtag = '';
+    },
+
+    addChosenHashtag: function(key) {
+      var index = this.availableHashtags.indexOf(key);
+      this.availableHashtags.splice(index, 1);
+      this.chosenHashtags.push(key);
+    },
+
+    removeChosenHashtag: function(key) {
+      var index = this.chosenHashtags.indexOf(key);
+      this.chosenHashtags.splice(index, 1);
+      this.availableHashtags.push(key);
+    },
+
+    stripHashtag: function(value) {
+      if (value.charAt(0) == '#') return value.substring(1);
+      return value;
+    }
+  },
+
+  computed: {
+    filteredAvailableHashtags: function() {
+      var self = this;
+      if (!this.newHashtag) return this.availableHashtags;
+      return this.availableHashtags.filter(function(v) {
+	var needle = self.stripHashtag(self.newHashtag),
+	    hayStack = self.stripHashtag(v);
+
+	return hayStack.indexOf(needle) !== -1;
+      });
+    }
   }
 });
